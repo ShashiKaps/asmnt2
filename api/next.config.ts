506cbd517a138d3ecb0f03e05af654db0c5ada7c
ShashiKaps <21704587@students.latrobe.edu.au>
2026-09-13 02:11:10 +0000

    code commit

--- api/next.config.ts
+++ api/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['172.31.78.43', 'localhost', '127.0.0.1'], // change to your IP in production
 };
 
 export default nextConfig;
