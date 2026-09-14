6d6f1c8ecac5f26f0212036fcd3c5ca67617a936
ShashiKaps <21704587@students.latrobe.edu.au>
2026-09-14 03:49:03 +0000

    commit code

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,11 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: [
-    '172.31.78.43',
-    'localhost',
-    '127.0.0.1',
-    'ec2-44-213-71-150.compute-1.amazonaws.com',
-  ], // change to your IP in production
+    '172.31.78.43', 'localhost', '127.0.0.1','ec2-100-31-7-207.compute-1.amazonaws.com',], // change to your IP in production
 };
 
 export default nextConfig;
